5f1903028a50a5e81517aef10edf11958a9973b0
Player1789 <jhoon5245@gmail.com>
2020-04-20 11:42:47 +1200

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,33 @@
-console.log('Client side javascript file is loaded');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const fetchWeather = (arg) => {
+	fetch(`/weather?address=${arg}`).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				return (messageOne.textContent = data.error);
+			}
 
-const fetchWeather = (arg)=>{
-    fetch(`/weather?address=${arg}`).then((response)=>{
-        response.json().then((data)=>{
-            if (data.error) {
-                return messageOne.textContent = data.error
-            }
-    
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-        })
-    })
-}
+			messageOne.textContent = data.location;
+			messageTwo.textContent = data.forecast;
+		});
+	});
+};
 
-weatherForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    const location = search.value
-    if (search.value===''){
-        messageOne.textContent = 'Please put an address'
-        messageTwo.textContent = ''
-    } else {
-        search.value =''
-    
-        messageOne.textContent = 'Loading'
-        messageTwo.textContent = ''
-    
-       fetchWeather(location)
-    }
-})
+weatherForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const location = search.value;
+	if (search.value === '') {
+		messageOne.textContent = 'Please put an address';
+		messageTwo.textContent = '';
+	} else {
+		search.value = '';
+
+		messageOne.textContent = 'Loading';
+		messageTwo.textContent = '';
+
+		fetchWeather(location);
+	}
+});
